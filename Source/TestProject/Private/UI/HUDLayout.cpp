f4da7830d0bbe4c707f9f162f847f424747a9639
David Ricardo <david.c.ricardo@gmail.com>
2022-09-10 22:23:35 +0100

    Added Icons on UI to open Profile and Inventory

--- Source/TestProject/Private/UI/HUDLayout.cpp
+++ Source/TestProject/Private/UI/HUDLayout.cpp
@@ -2,8 +2,68 @@
 
 
 #include "UI/HUDLayout.h"
+#include "Components/Border.h"
+#include "Components/Button.h"
+#include "MyPlayerController.h"
 
 void UHUDLayout::NativeConstruct()
 {
 	Super::NativeConstruct();
+
+	Btn_Inventory->OnReleased.AddUniqueDynamic(this, &UHUDLayout::OnBtnUIClicked);
+	Btn_Profile->OnReleased.AddUniqueDynamic(this, &UHUDLayout::OnBtnUIClicked);
+
+	Btn_Inventory->OnHovered.AddUniqueDynamic(this, &UHUDLayout::SlipUpInventory);
+	Btn_Profile->OnHovered.AddUniqueDynamic(this, &UHUDLayout::SlipUpProfile);
+
+	Btn_Inventory->OnUnhovered.AddUniqueDynamic(this, &UHUDLayout::SlipDownInventory);
+	Btn_Profile->OnUnhovered.AddUniqueDynamic(this, &UHUDLayout::SlipDownProfile);
+
+}
+
+void UHUDLayout::OnBtnUIClicked() {
+	if (AMyPlayerController* Controller = Cast<AMyPlayerController>(GetOwningPlayer()))
+	{
+		if (Btn_Inventory->IsHovered())
+		{
+			Controller->ToggleInventory();
+		}
+		else if (Btn_Profile->IsHovered())
+		{
+			Controller->ToggleProfile();
+		}
+	};
+}
+
+void UHUDLayout::SlipUpInventory()
+{
+	if (Btn_Inventory_Animation)
+	{
+		PlayAnimationForward(Btn_Inventory_Animation);
+	}
+}
+
+void UHUDLayout::SlipUpProfile()
+{
+	if (Btn_Profile_Animation)
+	{
+		PlayAnimationForward(Btn_Profile_Animation);
+	}
+}
+
+
+void UHUDLayout::SlipDownInventory()
+{
+	if (Btn_Inventory_Animation)
+	{
+		PlayAnimationReverse(Btn_Inventory_Animation);
+	}
+}
+
+void UHUDLayout::SlipDownProfile()
+{
+	if (Btn_Profile_Animation)
+	{
+		PlayAnimationReverse(Btn_Profile_Animation);		
+	}
 }
--- Source/TestProject/Public/UI/HUDLayout.h
+++ Source/TestProject/Public/UI/HUDLayout.h
@@ -6,8 +6,11 @@
 #include "Blueprint/UserWidget.h"
 #include "HUDLayout.generated.h"
 
+class UButton;
+class UBorder;
 class UTertiaryHUD;
 class UMainLayout;
+class UWidgetAnimation;
 /**
  * 
  */
@@ -18,12 +21,42 @@ class INVENTORYSYSTEMCPP_API UHUDLayout : public UUserWidget
 
 public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (BindWidget))
+	UBorder* Border_Profile;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (BindWidget))
+	UBorder* Border_Inventory;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (BindWidget))
+	UButton* Btn_Profile;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (BindWidget))
+	UButton* Btn_Inventory;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (BindWidget))
 	UMainLayout* MainLayout;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (BindWidget))
 	UTertiaryHUD* TertiaryHUD;
+
+	UPROPERTY(Transient, BlueprintReadWrite, meta = (BindWidgetAnim))
+	UWidgetAnimation* Btn_Inventory_Animation;
+	UPROPERTY(Transient, BlueprintReadWrite, meta = (BindWidgetAnim))
+	UWidgetAnimation* Btn_Profile_Animation;
 	
 protected:
 	UFUNCTION()
 	virtual void NativeConstruct() override;
+
+	UFUNCTION()
+	void SlipUpInventory();
+	UFUNCTION()
+	void SlipUpProfile();
+
+	UFUNCTION()
+	void SlipDownInventory();
+	UFUNCTION()
+	void SlipDownProfile();
+
+	UFUNCTION()
+	void OnBtnUIClicked();
 };
